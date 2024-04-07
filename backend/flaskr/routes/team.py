1082a2f7d1871483c436a211fd7f9c15ece6961d
Callum <git@callumclark.com>
2024-04-07 20:27:14 +0100

    added team routes

--- backend/flaskr/routes/team.py
+++ backend/flaskr/routes/team.py
@@ -1,11 +1,12 @@
 from flask import request, jsonify, Blueprint
 from extensions import db
-from flaskr.models import Team
+from flaskr.models import Team, League
 
 
 team = Blueprint("team", __name__)
 
 # CREATE - DONE
+#* DONT ALOW DUPE NAMES IN SAME LEAGUE
 @team.route("/api/v1/team/create", methods=["POST"])
 def create_team():
     data = request.json
@@ -50,20 +51,30 @@ def delete_team(team_id):
 
 
 # UPDATE - DONE
+# * issue if change name and league at same time if name is dupe 
 @team.route("/api/v1/team/<int:team_id>/update", methods=["PUT"])
 def update_team(team_id):
     team_data = Team.query.get(team_id)
 
     data = request.json
 
-    if "sport" in data:
-        team_data.sport = data.get("sport")
-
     if "name" in data:
-        team_data.team_name = data.get("team_name")
+        new_name = data.get("name")
+
+        unique_name_id = Team.query.filter_by(name=new_name, league_id=team_data.league_id).all()
+
+        if not unique_name_id:
+            team_data.name = new_name
+        else:
+            return jsonify({"error": "dupe team name in league"}), 500
 
     if "league_id" in data:
-        team_data.sport = data.get("league_id")
+        league = League.query.get(data.get("league_id"))
+
+        if league:
+            team_data.league_id = data.get("league_id")
+        else:
+            return jsonify({"error": "league doesnt exist"}), 500
 
     try:
         db.session.commit()
@@ -118,3 +129,31 @@ def single_team(team_id):
         return jsonify({"error": "Team not found"}), 404
     else:
         return jsonify(team), 200
+
+
+# GET ALL BY LOCATION/SPORT
+# ! BUGGY
+@team.route("/api/v1/team/location/<string:location>/sport/<string:sport>", methods=["GET"])
+def get_team_location(location, sport):
+    leagues = League.query.filter_by(location=location, sport=sport).all()
+
+    if leagues:
+
+        league_ids = [league.id for league in leagues]
+
+        teams_info = Team.query.filter(Team.league_id.in_(league_ids)).all()
+
+        team_data = [
+            {
+                "id": team.id,
+                "name": team.name,
+                "created": team.created_at,
+                "players": team.players,
+                "league": team.league_id
+            }
+            for team in teams_info
+        ]
+
+        return jsonify(team_data)
+
+    return jsonify({"error": f"No teams found in {location}"}), 404
